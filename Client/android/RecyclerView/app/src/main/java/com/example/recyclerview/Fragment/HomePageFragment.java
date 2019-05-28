85fb56dd440f899e526e245ffe9efc1b61664f1e
phuongph1997 <phuongph1997@gmail.com>
2019-05-28 21:54:04 +0700

    fix address order food

--- Client/android/RecyclerView/app/src/main/java/com/example/recyclerview/Fragment/HomePageFragment.java
+++ Client/android/RecyclerView/app/src/main/java/com/example/recyclerview/Fragment/HomePageFragment.java
@@ -1,5 +1,7 @@
 package com.example.recyclerview.Fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,6 +26,7 @@ import com.example.recyclerview.DTO.DataShopDTO;
 import com.example.recyclerview.MainActivity;
 import com.example.recyclerview.R;
 import com.example.recyclerview.CustomAdapter.ShopAdapter;
+import com.example.recyclerview.location;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,22 +38,56 @@ public class HomePageFragment extends Fragment {
     ArrayList<DataShopDTO> arrayListRestaurants = new ArrayList<>();
     RecyclerView RecyclerViewHorizontal;
     RecyclerView RecyclerViewVertical ;
-
+    TextView user_address;
+    private static final int REQUEST_CODE= 0x01;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
         View view = inflater.inflate(R.layout.home_page_fragment,parent,false);
         RecyclerViewHorizontal = view.findViewById(R.id.RView_RestaurantNearYou);
-
+        user_address = view.findViewById(R.id.tv_user_address);
         //initRecylerViewHorizontal();
         //initRecylerViewVertical();
         GetResturantData("a");
+
+        user_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), location.class);
+                startActivityForResult(intent, REQUEST_CODE);
+
+            }
+        });
         return view;
     }
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Kiểm tra requestCode có trùng với REQUEST_CODE vừa dùng
+        if(requestCode == REQUEST_CODE) {
+
+            // resultCode được set bởi DetailActivity
+            // RESULT_OK chỉ ra rằng kết quả này đã thành công
+            if(resultCode == Activity.RESULT_OK) {
+                // Nhận dữ liệu từ Intent trả về
+                final String result = data.getStringExtra("address");
+
+                // Sử dụng kết quả result bằng cách hiện Toast
+                if (!result.equals(""))
+                {
+                    user_address.setText(result);
+                }
+            } else {
+                // DetailActivity không thành công, không có data trả về.
+            }
+        }
     }
 
 
