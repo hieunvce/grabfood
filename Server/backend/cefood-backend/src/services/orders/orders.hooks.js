655af142104f9f425333eca2bf962968860bb74c
Hieu Nguyen <hieunv.ce@gmail.com>
2019-06-10 01:55:28 +0700

    Version 3.0.1

---
+++ Server/backend/cefood-backend/src/services/orders/orders.hooks.js
@@ -0,0 +1,55 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const accessFilter = async context => {
+  if (context.params.user != undefined && context.params.user){
+    context.data.userId = context.params.user._id;
+  } else {
+    error = new Error('Access denied.');
+    throw error;
+  }
+}
+
+const queryFilter = async context => {
+  if (context.params.user != undefined && context.params.user) {
+    context.params.query.userId = context.params.user._id;
+  } else {
+    error = new Error('Access denied.');
+    throw error;
+  }
+}
+
+const returnAllRecords = async context => {
+    context.params.paginate=false;
+}
+
+module.exports = {
+  before: {
+    all: [ authenticate('jwt')],
+    find: [queryFilter,returnAllRecords],
+    get: [queryFilter],
+    create: [accessFilter],
+    update: [accessFilter],
+    patch: [accessFilter],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
---
+++ Server/backend/cefood-backend/src/services/restaurants/restaurants.hooks.js
@@ -0,0 +1,43 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+const adminAccessOnlyFilter = async context => {
+  if (typeof (context.params.user.role) != undefined && context.params.user.role == 'admin') { 
+  } else {
+    error = new Error('Access denied.')
+    throw error;
+  }
+}
+const returnAllRecords = async context => {
+  context.params.paginate=false;
+}
+module.exports = {
+  before: {
+    all: [ ],
+    find: [returnAllRecords],
+    get: [],
+    create: [authenticate('jwt'),adminAccessOnlyFilter],
+    update: [authenticate('jwt'),adminAccessOnlyFilter],
+    patch: [authenticate('jwt'),adminAccessOnlyFilter],
+    remove: [authenticate('jwt'),adminAccessOnlyFilter]
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
