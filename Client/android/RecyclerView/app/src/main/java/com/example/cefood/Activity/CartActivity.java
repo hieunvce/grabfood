8638d1bdc0ee04b0a20fb8c16b7bb9eedcb8aa56
Hieu Nguyen <hieunv.ce@gmail.com>
2019-06-10 02:08:17 +0700

    verison 3.0.1

--- Client/android/RecyclerView/app/src/main/java/com/example/cefood/Activity/CartActivity.java
+++ Client/android/RecyclerView/app/src/main/java/com/example/cefood/Activity/CartActivity.java
@@ -48,6 +48,7 @@ public class CartActivity extends AppCompatActivity implements OnItemClick {
     String userAddress="Ho Chi Minh";
     TextView user_address;
     private static final int REQUEST_CODE = 0x01;
+    TextView comment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -99,64 +100,64 @@ public class CartActivity extends AppCompatActivity implements OnItemClick {
                 }
                 checkout();
 
-//                // Rating (Useless)
-//                Dialog myDialog;
-//                myDialog = new Dialog(CartActivity.this);
-//                myDialog.setContentView(R.layout.dialog_rating);
-//
-//                SmileRating smileRating = (SmileRating) myDialog.findViewById(R.id.smile_rating);
-//                comment = (TextView) myDialog.findViewById(R.id.txtComment);
-//                Button btnrating = (Button)myDialog.findViewById(R.id.btnrating);
-//
-//                smileRating.setOnSmileySelectionListener(new SmileRating.OnSmileySelectionListener() {
-//                    @Override
-//                    public void onSmileySelected(@BaseRating.Smiley int smiley, boolean reselected) {
-//                        // reselected is false when user selects different smiley that previously selected one
-//                        // true when the same smiley is selected.
-//                        // Except if it first time, then the value will be false.
-//                        switch (smiley) {
-//                            case SmileRating.BAD:
-//
-//                                comment.setText("App cần cải thiện hơn");
-//                                break;
-//                            case SmileRating.GOOD:
-//
-//                                comment.setText("App khá tốt");
-//                                break;
-//                            case SmileRating.GREAT:
-//
-//                                comment.setText("App Rất tuyệt vời");
-//                                break;
-//                            case SmileRating.OKAY:
-//
-//                                comment.setText("App tạm được");
-//                                break;
-//                            case SmileRating.TERRIBLE:
-//
-//                                comment.setText("App khá tệ");
-//                                break;
-//                        }
-//                    }
-//                });
-//
-//                myDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-//                    public void onCancel(DialogInterface dialog) {
-//                        Intent intent = new Intent(CartActivity.this, MainActivity.class);
-//                        finish();
-//                        startActivity(intent);
-//                    }
-//                });
-//
-//                btnrating.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent intent = new Intent(CartActivity.this, MainActivity.class);
-//                        finish();
-//                        startActivity(intent);
-//                    }
-//                });
-//                myDialog.show();
-//                // End Rating (Useless)
+                // Rating (Useless)
+                Dialog myDialog;
+                myDialog = new Dialog(CartActivity.this);
+                myDialog.setContentView(R.layout.dialog_rating);
+
+                SmileRating smileRating = (SmileRating) myDialog.findViewById(R.id.smile_rating);
+                comment = (TextView) myDialog.findViewById(R.id.txtComment);
+                Button btnrating = (Button)myDialog.findViewById(R.id.btnrating);
+
+                smileRating.setOnSmileySelectionListener(new SmileRating.OnSmileySelectionListener() {
+                    @Override
+                    public void onSmileySelected(@BaseRating.Smiley int smiley, boolean reselected) {
+                        // reselected is false when user selects different smiley that previously selected one
+                        // true when the same smiley is selected.
+                        // Except if it first time, then the value will be false.
+                        switch (smiley) {
+                            case SmileRating.BAD:
+
+                                comment.setText("App cần cải thiện hơn");
+                                break;
+                            case SmileRating.GOOD:
+
+                                comment.setText("App khá tốt");
+                                break;
+                            case SmileRating.GREAT:
+
+                                comment.setText("App Rất tuyệt vời");
+                                break;
+                            case SmileRating.OKAY:
+
+                                comment.setText("App tạm được");
+                                break;
+                            case SmileRating.TERRIBLE:
+
+                                comment.setText("App khá tệ");
+                                break;
+                        }
+                    }
+                });
+
+                myDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    public void onCancel(DialogInterface dialog) {
+                        Intent intent = new Intent(CartActivity.this, MainActivity.class);
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+
+                btnrating.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(CartActivity.this, MainActivity.class);
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                myDialog.show();
+                // End Rating
             }
         });
     }
@@ -238,8 +239,8 @@ public class CartActivity extends AppCompatActivity implements OnItemClick {
                         Log.d("Add order", "Add order success: " + response.code());
                         // TODO: Remove SharedPreferences
                         sharedPreferences.edit().remove("orderDetailArrayList").commit();
-                        Intent intent = new Intent(CartActivity.this, MainActivity.class);
-                        startActivity(intent);
+                        //Intent intent = new Intent(CartActivity.this, MainActivity.class);
+                        //startActivity(intent);
                     } else {
                         Log.d("Add order", "Add order Error.");
                         Toast.makeText(CartActivity.this, "Add order failed!", Toast.LENGTH_SHORT).show();
@@ -251,6 +252,11 @@ public class CartActivity extends AppCompatActivity implements OnItemClick {
                     Toast.makeText(CartActivity.this, "Add order failed!", Toast.LENGTH_SHORT).show();
                 }
             });
+            // Save location
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString("userAddress", userAddress);
+            editor.commit();
         }
     }
     @Override
